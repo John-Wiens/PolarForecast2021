07994c0beb832234ba9c390db2d4262068273ef3
jwiens <jwiens@neaeraconsulting.com>
2021-09-06 10:42:55 -0600

    Restructured app directory

--- app/src/app/dashboard/dashboard.component.ts
+++ app/src/app/dashboard/dashboard.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {FormControl} from '@angular/forms';
 import * as Chartist from 'chartist';
+import { ApiService } from '../services/api.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -10,12 +11,25 @@ import * as Chartist from 'chartist';
 export class DashboardComponent implements OnInit {
 
   myControl = new FormControl();
-  options: string[] = ['One', 'Two', 'Three'];
+  options: string[] = [];
+  
 
-  constructor() { }
+  constructor(private api: ApiService) { }
  
   
   ngOnInit() {
   }
+
+  /*
+  getOPtions() {
+    this.api.getEvents()
+      .subscribe(data => {
+        if ('teams' in data){
+          
+        }
+        
+      });
+      
+  }*/
 
 }
